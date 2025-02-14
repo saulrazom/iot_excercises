e6c89270f608b84da5318687861458fb9d14f100
URSUS32 <saul.razo@iteso.mx>
2025-02-13 18:46:05 -0600

    random tsp done

--- week04/sem4.py
+++ week04/sem4.py
@@ -1,5 +1,6 @@
 #import random
-from random import uniform
+from random import uniform, shuffle, seed
+from time import time
 
 
 def random_net(n):
@@ -14,12 +15,12 @@ net2 = [[0, 4, 2, 1, 3, 5, 5, 6, 8, 6], [3, 0, 6, 5, 7, 2, 2, 5, 8, 2], [2, 3, 0
 net3 = random_net(11)  # min_distance = ??
 net  = net2
 
-
-def process(st, k, visited, solution, d):
+#### Algoritmo exhaustivo
+def process(st, k, visited, solution, d, nt):
     # Si k = n, ya tenemos la permutación completa y podemos devolver la distancia total
-    n = len(net)
+    n = len(nt)
     if k == n:
-        return d + net[solution[k - 1]][st]
+        return d + nt[solution[k - 1]][st]
     #Si no, tenemos que intentar todas las posibilidades para la posición k, y devolver la mínima distancia de todas.
     minD = float('inf')
     for i in range(n):
@@ -28,15 +29,12 @@ def process(st, k, visited, solution, d):
             solution[k] = i
             # d = process(st, k+1, ...)
             # minD = min(minD, d)
-            minD = min(minD, process(st, k + 1, visited, solution, d + net[solution[k - 1]][solution[k]]))
+            minD = min(minD, process(st, k + 1, visited, solution, d + nt[solution[k - 1]][solution[k]], nt))
             visited[i] = False
     return minD
    
-
-
-
-def tsp_exhaustive(st):
-    n = len(net)
+def tsp_exhaustive(st, nt):
+    n = len(nt)
     # Si st = 2, n = 6
     # ¿ Cómo generamos estas listas por "Comprensión"?
     # visited = [ False, False, True, False, False, False] --> Tamaño N
@@ -44,9 +42,10 @@ def tsp_exhaustive(st):
     # Llamar a process pasando estos datos con k = 1, d = 0
     visited = [False if i != st else True for i in range(n)]
     solution = [st if i == 0 or i == n else -1 for i in range(n + 1)]
-    return process(st, 1, visited, solution, 0)
+    return process(st, 1, visited, solution, 0, nt)
     
 
+#### Algoritmo vecino más cercano
 def get_nearest(st, visited):
     return 0
 
@@ -55,18 +54,49 @@ def tsp_nearest_neighbor(start):
     return 0
 
 
-def eval(start, solution):
-    return 0
+#### Algoritmo aleatorio
+def eval(start, solution, nt):
+    #calcular la distancia de la solución/secuencia utilizando G
+    d = 0
+    n = len(nt)
+    for i in range(n - 1):
+        d += nt[solution[i]][solution[i + 1]]
+    d += nt[solution[-1]][start]
+    return d
+   
+def tsp_random(start, m, nt): # m = iterations
+    n = len(nt)
+    # Solución inicial [0, 1, 2.., N – 2] reemplazando S por N – 1
+    solution = [i for i in range(n) if i != start]
+    solution.append(start)
 
+    #Evaluar la solución
+    minD = eval(start, solution, nt)
 
-def tsp_random(start, n):
-    return 0
+    # Repetir m veces
+    for _ in range(m):
+        # Revolver la secuencia anterior (shuffle)
+        shuffle(solution[:-1])
+        # Calcular la distancia de la nueva secuencia aleatoria
+        d = eval(start, solution, nt)
+        # Actualizar la distancia mínima, si aplica
+        if d < minD:
+            minD = d
 
+    return minD
 
 if __name__ == '__main__':
-    minD = tsp_random(0, 10000)
-    print(minD)
-    minD = tsp_exhaustive(2)
-    print(minD)
+    start = time()
+    minD = tsp_random(0, 100000, net)
+    end = time()
+    print(f"\nTSP RANDOM\nMin distance: {minD}\n{(end - start):.2f} seconds")
+
+    start = time()
+    minD = tsp_exhaustive(2, net)
+    end = time()
+    print(f"\nTSP EXHAUSTIVE\nMin distance: {minD}\n{(end - start):.2f} seconds")
+
+    start = time()
     minD = tsp_nearest_neighbor(0)
-    print(minD)
+    end = time()
+    print(f"\nTSP NEAREST NEIGHBOR\nMin distance: {minD}\n{(end - start):.2f} seconds")
